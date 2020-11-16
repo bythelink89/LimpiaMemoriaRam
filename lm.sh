ac85dfb04f31532c4259799f9b851aebc4db8609
Evan <72405806+bythelink89@users.noreply.github.com>
2020-11-15 19:01:55 -0600

    Create lm.sh

---
+++ lm.sh
@@ -0,0 +1,16 @@
+echo “Limpiando la caché“;
+sync ; echo 3 > /proc/sys/vm/drop_caches
+echo “Limpiando la caché“;
+sync ; echo 3 > /proc/sys/vm/drop_caches
+echo “Limpiando la caché“;
+sync ; echo 3 > /proc/sys/vm/drop_caches
+echo “Limpiando la caché“;
+sync ; echo 3 > /proc/sys/vm/drop_caches
+echo “Limpiando la caché“;
+sync ; echo 3 > /proc/sys/vm/drop_caches
+echo “Limpiando la caché“;
+sync ; echo 3 > /proc/sys/vm/drop_caches
+echo “Limpiando la caché“;
+sync ; echo 3 > /proc/sys/vm/drop_caches
+echo “Limpiando la caché“;
+sync ; echo 3 > /proc/sys/vm/drop_caches
